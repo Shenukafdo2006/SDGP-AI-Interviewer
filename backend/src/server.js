28945a937b8df8af409882dd315dffd83ea95c27
Shenukafdo2006 <shenukafdo2006@gmail.com>
2026-03-04 13:10:56 +0530

    update server js for interview training

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const { admin, initCollections, getOrCreateUser } = require("./config/firebase")
 const aiInterviewRoutes = require("./routes/aiInterviewRoutes");
 
 const app  = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
