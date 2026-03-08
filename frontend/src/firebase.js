2ea330f7a2c7c26ab4a366135fd13ef7128c3e53
charukasomawansa <csomawansa855@gmail.com>
2026-03-09 02:31:30 +0530

    fixed achivements frontend and Database

---
+++ frontend/src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyChtXN6kk7T3kmDlHqL1EW-ugpKSrd6khg",
+  authDomain:        "revolve-software-75233.firebaseapp.com",
+  projectId:         "revolve-software-75233",
+  storageBucket:     "revolve-software-75233.firebasestorage.app",
+  messagingSenderId: "501631467649",
+  appId:             "1:501631467649:web:bccbbaad14d9d20e57cd80",
+  measurementId:     "G-C5M9YT21W0",
+};
+
+const app       = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
